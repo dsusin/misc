f00d11c021e6d779248fafc0dcaa13891a65b211
dsusin <dsusin@bdigital.org>
2013-10-24 11:54:05 +0200

    Updated parsing

--- alexascrapper.py
+++ alexascrapper.py
@@ -2,9 +2,16 @@ from BeautifulSoup import BeautifulSoup
 import urllib2
 
 for i in range(20):
-	url='http://www.alexa.com/topsites/countries;'+str(i)+'/ES'
+#	url='http://www.alexa.com/topsites/countries;'+str(i)+'/ES'
+	url='http://www.alexa.com/topsites/global;'+str(i)
 	page=urllib2.urlopen(url)
 	soup=BeautifulSoup(page.read())
-	sites=soup.findAll('span', {'class':'small topsites-label'})
-	for site in sites:
-		print(site.string)
+        
+        #old parsing
+#	sites=soup.findAll('span', {'class':'small topsites-label'})
+#	for site in sites:
+#		print(site.string)
+
+        sites=soup.findAll('h2')
+        for site in sites:
+            print site.a.string
